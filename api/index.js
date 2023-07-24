838852af6d9ccde08af65f332434a24953004751
Abhay Gupta <abhaysg2000@gmail.com>
2023-07-25 02:47:26 +0530

    initial commit

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+	.then(() => console.log("DB connection successful"))
+	.catch((err) => {
+		console.log(err)
+	});
+
+// app.get("/api/test", () => {
+// 	console.log("Test is successful!")
+// })
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log("Backend server up and running...");
+});
