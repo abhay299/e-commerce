a766b69815b44a9a49c414b5489fd7584e797b72
Abhay Gupta <abhaysg2000@gmail.com>
2023-07-31 16:57:04 +0530

    apiCalls update

--- client/src/redux/apiCalls.js
+++ client/src/redux/apiCalls.js
@@ -4,7 +4,7 @@ import { loginFailure, loginStart, loginSuccess } from "./userRedux"
 export const login = async (dispatch, user) => {
 	dispatch(loginStart());
 	try {
-		const res = await publicRequest.post("/auth/login", user);
+		const res = await publicRequest.post("https://e-commerce-backend-nu-steel.vercel.app/api/auth/login", user);
 		console.log(res);
 		dispatch(loginSuccess(res.data));
 	} catch (err) {
