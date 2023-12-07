ebf2e15f4e0edad808686fe979c644cd875967a6
abhay299 <abhaysg2000@gmial.com>
2023-12-07 06:09:30 +0530

    commit-ad

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -1,66 +1,71 @@
-import { Send } from '@material-ui/icons';
-import React from 'react';
-import { styled } from 'styled-components';
-import { mobile } from '../responsive';
+import { Send } from "@material-ui/icons";
+import React from "react";
+import { styled } from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
-	font-family: 'Mulish', sans-serif;
-	height: 60vh;
-	background-color: #fcf5f5;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
+  font-family: "Mulish", sans-serif;
+  height: 60vh;
+  background-color: #fcf5f5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `;
 
 const Title = styled.h1`
-	font-size: 70px;
-	margin-bottom: 20px;
+  font-size: 70px;
+  margin-bottom: 20px;
 `;
 
 const Desc = styled.div`
-	font-size: 24px;
-	font-weight: 300;
-	margin-bottom: 20px;
-	${mobile({ testAlign: "center" })};
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom: 20px;
+  ${mobile({ testAlign: "center" })};
 `;
 
 const InputContainer = styled.div`
-	width: 50%;
-	height: 40px;
-	background-color: #white;
-	display: flex;
-	justify-content: space-between;
-	border: 1px solid lightgray;
-	${mobile({ width: "80%" })};
+  width: 50%;
+  height: 40px;
+  background-color: #white;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid lightgray;
+  ${mobile({ width: "80%" })};
 `;
 
 const Input = styled.input`
-	border: none;
-	flex: 8;
-	padding-left:20px;
+  border: none;
+  flex: 8;
+  padding-left: 20px;
 `;
 
 const Button = styled.button`
-	flex: 1;
-	border:none;
-	background-color: teal;
-	color: white;
+  flex: 1;
+  border: none;
+  background-color: teal;
+  color: white;
 `;
 
 const Newsletter = () => {
-	return (
-		<Container>
-			<Title>Newsletter</Title>
-			<Desc>Get timely updates of your favourite products</Desc>
-			<InputContainer placeholder='Your Email Address'>
-				<Input />
-				<Button>
-					<Send />
-				</Button>
-			</InputContainer>
-		</Container>
-	)
-}
+  return (
+    <Container>
+      <Title>Newsletter</Title>
+      <Desc>Get timely updates of your favourite products</Desc>
+      <InputContainer placeholder="Your Email Address">
+        <Input />
+        <Button>
+          <Send />
+        </Button>
+      </InputContainer>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5113933315551343"
+        crossorigin="anonymous"
+      ></script>
+    </Container>
+  );
+};
 
 export default Newsletter;
